4360ee08bc7c4165463a7c1d549560a9b32bed1e
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-01-06 19:16:00 +0700

    feat(ui): add headline component

---
+++ components/feature/headline.tsx
@@ -0,0 +1,7 @@
+export default function Headline() {
+  return (
+    <h2 className="text-center font-primary text-headline leading-headline font-semibold">
+      How&#39;s the sky looking today?
+    </h2>
+  );
+}
