7a1fe64bfd5511257161b87a05818adf6f21b420
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-01-06 19:41:55 +0700

    feat(ui): allow additional classname on headline

--- components/feature/headline.tsx
+++ components/feature/headline.tsx
@@ -1,6 +1,13 @@
-export default function Headline() {
+import clsx from "clsx";
+
+export default function Headline({ className }: { className?: string }) {
   return (
-    <h2 className="text-center font-primary text-headline leading-headline font-semibold">
+    <h2
+      className={clsx(
+        "text-center font-primary text-headline leading-headline font-semibold",
+        className,
+      )}
+    >
       How&#39;s the sky looking today?
     </h2>
   );
