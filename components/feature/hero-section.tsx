ab54a6aed98da6004b98a9200e595213d6dbe333
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-01-07 20:41:14 +0700

    feat(ui): add hero section component

---
+++ components/feature/hero-section.tsx
@@ -0,0 +1,27 @@
+import clsx from "clsx";
+import Image from "next/image";
+
+export default function HeroSection({ className }: { className?: string }) {
+  return (
+    <div
+      className={clsx(
+        "flex h-hero-section flex-col items-center justify-center rounded-xl bg-[url(@/public/bg-today-small.svg)] bg-cover bg-center",
+        className,
+      )}
+    >
+      <div className="text-center">
+        <h2 className="text-hero-section-city font-semibold">
+          Berlin, Germany
+        </h2>
+        <p className="text-text-secondary">Tuesday, Aug 5, 2025</p>
+      </div>
+      <div className="flex items-center">
+        <Image src="/icon-sunny.webp" alt="sunny" width={110} height={110} />
+        <p className="sr-only">Sunny</p>
+        <p className="text-hero-section-degree font-semibold">
+          <span className="inline-block -skew-x-8">20</span>&deg;
+        </p>
+      </div>
+    </div>
+  );
+}
