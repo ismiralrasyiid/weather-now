23c85049d9897e4703653310f0107619ef3979f1
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-01-10 21:35:02 +0700

    feat(ui): add responsive behavior to menu dropdown trigger

--- components/feature/menu-dropdown/root.tsx
+++ components/feature/menu-dropdown/root.tsx
@@ -5,22 +5,40 @@ export default function MenuDropdown() {
   return (
     <Menu.Root>
       <Menu.Trigger
-        className="flex cursor-pointer items-center gap-1.5 rounded-md bg-background-primary p-2 text-xs"
+        className="flex cursor-pointer items-center gap-1.5 rounded-md bg-background-primary p-2 text-xs lg:gap-2.5 lg:px-3.25 lg:text-sm"
         openOnHover
       >
         <Image
+          className="lg:hidden"
           src="/icon-units.svg"
           alt=""
           width={13}
           height={13}
           aria-hidden
         />
+        <Image
+          className="hidden lg:block"
+          src="/icon-units.svg"
+          alt=""
+          width={15}
+          height={15}
+          aria-hidden
+        />
         Units
         <Image
+          className="lg:hidden"
           src="/icon-dropdown.svg"
           alt=""
           width={9}
           height={9}
+          aria-hidden
+        />
+        <Image
+          className="hidden lg:block"
+          src="/icon-dropdown.svg"
+          alt=""
+          width={12}
+          height={12}
           aria-hidden
         />
       </Menu.Trigger>
