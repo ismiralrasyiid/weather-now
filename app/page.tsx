770e0cd0aaf141fd860d014ae14279761fe54266
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-01-10 17:57:10 +0700

    feat(page): integrate existing components into page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,58 @@
+import { DailyForecasts } from "@/components/feature/daily-forecasts";
+import { DailyForecast } from "@/components/feature/daily-forecasts/forecast";
+import Headline from "@/components/feature/headline";
+import HeroSection from "@/components/feature/hero-section";
+import { HourlyForecasts } from "@/components/feature/hourly-forecasts";
+import { HourlyForecast } from "@/components/feature/hourly-forecasts/forecast";
+import { Searchbar } from "@/components/feature/searchbar";
+import { WeatherDetails } from "@/components/feature/weather-details";
+import { MetricData } from "@/components/feature/weather-details/metric";
+import Navbar from "@/components/layout/navbar";
+
+const mockData = {
+  metrics: Array.from<MetricData>({ length: 4 }).fill({
+    id: "",
+    name: "Feels like",
+    value: 18,
+    unit: "°",
+  }),
+  dailyForecasts: Array.from<DailyForecast>({ length: 7 }).fill({
+    id: "",
+    day: "Tue",
+    url: "/icon-sunny.webp",
+    highestTemp: 20,
+    lowestTemp: 14,
+    indicator: "Sunny",
+  }),
+  hourlyForecasts: Array.from<HourlyForecast>({ length: 24 }).fill({
+    id: "",
+    hour: "3 PM",
+    url: "/icon-overcast.webp",
+    indicator: "Cloudy",
+    temperature: 20,
+  }),
+};
+
 export default function Home() {
   return (
-    <div>
-      Init
-    </div>
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Headline className="mt-9.5" />
+        <Searchbar className="mt-8.5" />
+        <HeroSection className="mt-7" />
+        <WeatherDetails className="mt-5" metrics={mockData.metrics} />
+        <DailyForecasts
+          className="mt-6.5"
+          forecasts={mockData.dailyForecasts}
+        />
+        <HourlyForecasts
+          className="mt-7.5"
+          forecasts={mockData.hourlyForecasts}
+        />
+      </main>
+    </>
   );
 }
