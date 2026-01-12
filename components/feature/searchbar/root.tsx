213bf6a1042330d926467075d6981464362d861d
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-01-12 21:55:43 +0700

    refactor(ui): rename button component file

--- components/feature/searchbar/root.tsx
+++ components/feature/searchbar/root.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Button from "@/components/ui/Button";
+import Button from "@/components/ui/button";
 import { Autocomplete, Form } from "@base-ui/react";
 import clsx from "clsx";
 import Image from "next/image";
