7b018053924b5ec1164dab8ee7e8f8ca64340119
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-02-09 14:32:07 +0700

    feat(unit): generate switchUnitTypeParam inside useUnitTypeParam hook

--- hooks/use-unit-type-param.ts
+++ hooks/use-unit-type-param.ts
@@ -1,10 +1,18 @@
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { parseUnitType, UnitType } from "@/domains/unit";
 
 export function useUnitTypeParam() {
+  const pathname = usePathname();
   const searchParams = useSearchParams();
+
   const currentUnitType: UnitType = parseUnitType(searchParams.get("unitType"));
   const toSwitchUnitType: UnitType =
     currentUnitType === UnitType.Metric ? UnitType.Imperial : UnitType.Metric;
-  return { currentUnitType, toSwitchUnitType };
+
+  const newSearchParams = new URLSearchParams(searchParams.toString());
+  newSearchParams.set("unitType", toSwitchUnitType);
+
+  const switchUnitTypeHref = `${pathname}?${newSearchParams.toString()}`;
+
+  return { currentUnitType, toSwitchUnitType, switchUnitTypeHref };
 }
