0e3833dd05d0617a386b7a33c7cd814e54b15008
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-03-12 12:36:04 +0700

    feat(weather): add weather domain

---
+++ domains/weather/constants.ts
@@ -0,0 +1,137 @@
+import { WeatherMap } from "./types";
+
+export const openMeteoCurrentWeatherVariables = [
+  "temperature_2m",
+  "relative_humidity_2m",
+  "wind_speed_10m",
+  "precipitation",
+  "weather_code",
+  "apparent_temperature",
+].join(",");
+
+export const openMeteoHourlyWeatherVariables = [
+  "temperature_2m",
+  "weather_code",
+].join(",");
+
+export const openMeteoDailyWeatherVariables = [
+  "weather_code",
+  "temperature_2m_max",
+  "temperature_2m_min",
+].join(",");
+
+export const weatherMap: WeatherMap = {
+  0: {
+    description: "Sunny",
+    image: "/icon-sunny.webp",
+  },
+
+  1: {
+    description: "Mainly Sunny",
+    image: "/icon-sunny.webp",
+  },
+  2: {
+    description: "Partly Cloudy",
+    image: "/icon-partly-cloudy.webp",
+  },
+  3: {
+    description: "Cloudy",
+    image: "/icon-overcast.webp",
+  },
+  45: {
+    description: "Foggy",
+    image: "/icon-fog.webp",
+  },
+  48: {
+    description: "Rime Fog",
+    image: "/icon-fog.webp",
+  },
+  51: {
+    description: "Light Drizzle",
+    image: "/icon-drizzle.webp",
+  },
+  53: {
+    description: "Drizzle",
+    image: "/icon-drizzle.webp",
+  },
+  55: {
+    description: "Heavy Drizzle",
+    image: "/icon-drizzle.webp",
+  },
+  56: {
+    description: "Light Freezing Drizzle",
+    image: "/icon-drizzle.webp",
+  },
+  57: {
+    description: "Freezing Drizzle",
+    image: "/icon-drizzle.webp",
+  },
+  61: {
+    description: "Light Rain",
+    image: "/icon-rain.webp",
+  },
+  63: {
+    description: "Rain",
+    image: "/icon-rain.webp",
+  },
+  65: {
+    description: "Heavy Rain",
+    image: "/icon-rain.webp",
+  },
+  66: {
+    description: "Light Freezing Rain",
+    image: "/icon-rain.webp",
+  },
+  67: {
+    description: "Freezing Rain",
+    image: "/icon-rain.webp",
+  },
+  71: {
+    description: "Light Snow",
+    image: "/icon-snow.webp",
+  },
+  73: {
+    description: "Snow",
+    image: "/icon-snow.webp",
+  },
+  75: {
+    description: "Heavy Snow",
+    image: "/icon-snow.webp",
+  },
+  77: {
+    description: "Snow Grains",
+    image: "/icon-snow.webp",
+  },
+  80: {
+    description: "Light Showers",
+    image: "/icon-rain.webp",
+  },
+  81: {
+    description: "Showers",
+    image: "/icon-rain.webp",
+  },
+  82: {
+    description: "Heavy Showers",
+    image: "/icon-rain.webp",
+  },
+  85: {
+    description: "Light Snow Showers",
+    image: "/icon-snow.webp",
+  },
+  86: {
+    description: "Snow Showers",
+    image: "/icon-snow.webp",
+  },
+  95: {
+    description: "Thunderstorm",
+    image: "/icon-storm.webp",
+  },
+  96: {
+    description: "Light Thunderstorms With Hail",
+    image: "/icon-storm.webp",
+  },
+  99: {
+    description: "Thunderstorm With Hail",
+    image: "/icon-storm.webp",
+  },
+};
---
+++ domains/weather/types.ts
@@ -0,0 +1,78 @@
+export type OpenMeteoWeatherResponse = Readonly<{
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_units: OpenMeteoCurrentUnits;
+  current: OpenMeteoCurrent;
+  hourly_units: OpenMeteoHourlyUnits;
+  hourly: OpenMeteoHourly;
+  daily_units: OpenMeteoDailyUnits;
+  daily: OpenMeteoDaily;
+}>;
+
+export type OpenMeteoCurrentUnits = Readonly<{
+  time: string;
+  interval: string;
+  temperature_2m: string;
+  relative_humidity_2m: string;
+  wind_speed_10m: string;
+  precipitation: string;
+  weather_code: string;
+  apparent_temperature: string;
+}>;
+
+export type OpenMeteoCurrent = Readonly<{
+  time: string;
+  interval: number;
+  temperature_2m: number;
+  relative_humidity_2m: number;
+  wind_speed_10m: number;
+  precipitation: number;
+  weather_code: number;
+  apparent_temperature: number;
+}>;
+
+export type OpenMeteoHourlyUnits = Readonly<{
+  time: string;
+  temperature_2m: string;
+  weather_code: string;
+}>;
+
+export type OpenMeteoHourly = Readonly<{
+  time: ReadonlyArray<string>;
+  temperature_2m: ReadonlyArray<number>;
+  weather_code: ReadonlyArray<number>;
+}>;
+
+export type OpenMeteoDailyUnits = Readonly<{
+  time: string;
+  weather_code: string;
+  temperature_2m_max: string;
+  temperature_2m_min: string;
+}>;
+
+export type OpenMeteoDaily = Readonly<{
+  time: ReadonlyArray<string>;
+  weather_code: ReadonlyArray<number>;
+  temperature_2m_max: ReadonlyArray<number>;
+  temperature_2m_min: ReadonlyArray<number>;
+}>;
+
+type CurrentWeatherKeys = "current" | "current_units";
+
+export type CurrentWeather = Pick<OpenMeteoWeatherResponse, CurrentWeatherKeys>;
+
+export type DailyWeather = OpenMeteoDaily;
+
+export type HourlyWeather = OpenMeteoHourly;
+
+export type WeatherInfo = {
+  description: string;
+  image: string;
+};
+
+export type WeatherMap = Record<number, WeatherInfo>;
