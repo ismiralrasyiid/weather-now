9bedb8f60ce1412bb37c0931fdf198376f0c4147
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-03-15 17:02:33 +0700

    refactor(ui): display metric name at WeatherDetailsSkeleton

--- components/feature/skeleton/weather-details.tsx
+++ components/feature/skeleton/weather-details.tsx
@@ -4,9 +4,19 @@ import clsx from "clsx";
 export function WeatherDetailsSkeleton({ className }: { className?: string }) {
   return (
     <div className={clsx("grid grid-cols-2 gap-4 sm:grid-cols-4", className)}>
-      {Array.from({ length: 4 }).map((_, index) => (
-        <Skeleton className="h-26.25" key={`metric-skeleton-${index}`} />
-      ))}
+      {["Feels like", "Humidity", "Wind", "Precipitation"].map(
+        (metric, index) => (
+          <Skeleton
+            className="h-26.25 rounded-xl p-3.5"
+            key={`metric-skeleton-${index}`}
+          >
+            <p className="flex flex-col gap-4">
+              <span className="font-medium text-text-tertiary">{metric}</span>
+              <span className="text-3xl font-extralight">-</span>
+            </p>
+          </Skeleton>
+        ),
+      )}
     </div>
   );
 }
