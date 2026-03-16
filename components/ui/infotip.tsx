342ccaa7921fe87a467d883b7794c0db514a325d
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-03-17 00:15:52 +0700

    feat(ui): add Infotip reusable component

---
+++ components/ui/infotip.tsx
@@ -0,0 +1,59 @@
+import { Popover } from "@base-ui/react";
+import clsx from "clsx";
+
+const { Root, Trigger, Portal, Positioner, Popup, Description, Arrow } =
+  Popover;
+
+export default function Infotip({
+  children,
+  description,
+  forOverview,
+}: {
+  children: React.ReactNode;
+  description: string;
+  forOverview?: boolean;
+}) {
+  return (
+    <Root>
+      <Trigger openOnHover delay={30}>
+        {children}
+      </Trigger>
+      <Portal>
+        <Positioner side="top" sideOffset={forOverview ? -15 : 0}>
+          <Popup
+            className={clsx("max-w-35 rounded-md border shadow-xl", {
+              "border-accent-primary bg-accent-primary px-2.5 py-1.5":
+                forOverview,
+              "border-border bg-background-primary px-1.5 py-1": !forOverview,
+            })}
+          >
+            <Description className="text-center text-xs text-text-primary">
+              {description}
+            </Description>
+            <Arrow className="-bottom-1.25">
+              <svg width="12" height="7" viewBox="0 0 12 7">
+                <path
+                  className={clsx({
+                    "stroke-accent-primary": forOverview,
+                    "stroke-border": !forOverview,
+                  })}
+                  d="M1 1L6 6L11 1"
+                  fill="none"
+                  strokeWidth="1"
+                  strokeLinejoin="round"
+                />
+                <path
+                  className={clsx({
+                    "fill-accent-primary": forOverview,
+                    "fill-background-primary": !forOverview,
+                  })}
+                  d="M1 1L6 6L11 1L1 1Z"
+                />
+              </svg>
+            </Arrow>
+          </Popup>
+        </Positioner>
+      </Portal>
+    </Root>
+  );
+}
