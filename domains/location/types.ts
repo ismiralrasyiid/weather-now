39f08a9cb02971c171dd414a51d0f7478b957413
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-04-02 12:18:23 +0700

    refactor(location): set admin1 and admin1_id as optional prop

--- domains/location/types.ts
+++ domains/location/types.ts
@@ -6,12 +6,12 @@ export type OpenMeteoLocation = Readonly<{
   elevation: number;
   feature_code: string;
   country_code: string;
-  admin1_id: number;
+  admin1_id?: number;
   admin2_id?: number;
   timezone: string;
   country_id: number;
   country: string;
-  admin1: string;
+  admin1?: string;
   admin2?: string;
 }>;
 
