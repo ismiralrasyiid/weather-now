eb73ba025004cd587319875995e2cde2f9eedde8
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-04-02 15:33:16 +0700

    feat(homepage): add PopularCities section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Headline from "@/components/feature/headline";
+import { PopularCities } from "@/components/feature/popular-cities";
 import { Searchbar } from "@/components/feature/searchbar";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
           How&#39;s the sky looking today?
         </Headline>
         <Searchbar className="mt-8.5 md:mt-13.5" />
+        <PopularCities className="mt-6.5" />
       </main>
     </>
   );
