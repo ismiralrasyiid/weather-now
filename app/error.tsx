e67da180f2fbdd31b3327ff7fcfdd0034aa02485
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-04-02 15:30:47 +0700

    fix(errorpage): correct icon paths and refine error message

--- app/error.tsx
+++ app/error.tsx
@@ -22,7 +22,7 @@ export default function Error({
     <div className="mt-25 flex flex-col items-center gap-4">
       <Image
         className="mx-auto"
-        src="icon-error.svg"
+        src="/icon-error.svg"
         alt=""
         width={38}
         height={38}
@@ -30,14 +30,19 @@ export default function Error({
       />
       <Headline>Something went wrong</Headline>
       <p className="max-w-120 text-center text-lg text-text-secondary">
-        We couldn&#39;t connect to the server &#40;API error&#41;. Please try
-        again in a few moments
+        We couldn&#39;t complete your request. Please try again in a few moments
       </p>
       <button
         className="flex cursor-pointer items-center gap-2.5 rounded-lg bg-background-primary px-3.5 py-2.5"
         onClick={reset}
       >
-        <Image src="icon-retry.svg" alt="" width={15} height={15} aria-hidden />
+        <Image
+          src="/icon-retry.svg"
+          alt=""
+          width={15}
+          height={15}
+          aria-hidden
+        />
         <span className="text-sm">Retry</span>
       </button>
     </div>
