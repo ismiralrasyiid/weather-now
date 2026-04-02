2f3db340b342d325214e71e1a7ebd0701847958a
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-04-02 15:23:27 +0700

    refactor(errorpage): apply native errorpage props to handle error properly

--- app/error.tsx
+++ app/error.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import Headline from "@/components/feature/headline";
 
-export default function Error() {
-  const router = useRouter();
+function logError(error: Error) {
+  if (process.env.NODE_ENV === "development") {
+    console.error(error);
+  }
+}
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  logError(error);
 
   return (
     <div className="mt-25 flex flex-col items-center gap-4">
@@ -24,7 +35,7 @@ export default function Error() {
       </p>
       <button
         className="flex cursor-pointer items-center gap-2.5 rounded-lg bg-background-primary px-3.5 py-2.5"
-        onClick={router.refresh}
+        onClick={reset}
       >
         <Image src="icon-retry.svg" alt="" width={15} height={15} aria-hidden />
         <span className="text-sm">Retry</span>
