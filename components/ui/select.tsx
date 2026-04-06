2f1afcae0b1a61a21db9019c9ceb4c5a3dcd9dab
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-04-07 00:15:38 +0700

    style(select): add transition

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -31,7 +31,7 @@ export default function Select<T>(props: SelectProps<T>) {
     <Root items={items} value={value} onValueChange={onChange}>
       <Trigger
         className={clsx(
-          "flex cursor-pointer items-center gap-3.5 rounded-lg bg-background-tertiary px-4 py-2 text-sm",
+          "group flex cursor-pointer items-center gap-3.5 rounded-lg bg-background-tertiary px-4 py-2 text-sm",
           className,
         )}
         aria-label={`${value}`}
@@ -39,6 +39,7 @@ export default function Select<T>(props: SelectProps<T>) {
         <Value />
         <Icon>
           <Image
+            className="size-3 transition-transform duration-200 ease-in group-data-pressed:rotate-180"
             src="/icon-dropdown.svg"
             alt="Icon Dropdown"
             width={12}
@@ -49,13 +50,13 @@ export default function Select<T>(props: SelectProps<T>) {
       </Trigger>
       <Portal>
         <Positioner alignItemWithTrigger={false} align="end" sideOffset={10}>
-          <Popup>
+          <Popup className="origin-top transition-[transform,scale,opacity] data-ending-style:scale-90 data-ending-style:opacity-0 data-starting-style:scale-90 data-starting-style:opacity-0">
             <List className="flex min-w-select cursor-pointer flex-col gap-1 rounded-lg border border-border bg-background-primary p-1.5">
               {items.map(({ label, value }) => (
                 <Item
                   key={label}
                   value={value}
-                  className="rounded-lg p-1.5 text-text-primary hover:bg-background-secondary-hover data-selected:bg-background-secondary"
+                  className="rounded-lg p-1.5 text-text-primary transition-all duration-150 hover:translate-x-0.5 hover:bg-background-secondary-hover data-selected:bg-background-secondary data-selected:hover:bg-background-secondary-hover"
                 >
                   <ItemText>{label}</ItemText>
                 </Item>
