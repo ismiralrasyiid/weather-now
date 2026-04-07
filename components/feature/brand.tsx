0e235fe6c90ef61d8ffad234e3f3590eac759a2f
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-04-07 10:15:45 +0700

    feat(brand): make brand link to homepage

--- components/feature/brand.tsx
+++ components/feature/brand.tsx
@@ -1,25 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Brand() {
   return (
-    <div>
+    <Link href="/">
       <h1 className="sr-only">Weather Now</h1>
       <Image
-        className="md:hidden"
-        src="/logo.svg"
-        alt=""
-        height={30}
-        width={125}
-        aria-hidden
-      />
-      <Image
-        className="hidden md:block"
+        className="h-7.5 w-31.25 md:h-9 md:w-43.75"
         src="/logo.svg"
         alt=""
         height={36}
         width={175}
         aria-hidden
       />
-    </div>
+    </Link>
   );
 }
