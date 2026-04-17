07de00d71bacaf88e49c730d76235514cd81a17a
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-04-17 14:42:51 +0700

    feat(unit): add unit symbol parsers

--- domains/unit/parser.ts
+++ domains/unit/parser.ts
@@ -1,6 +1,27 @@
 import { UnitType } from "./constants";
+import {
+  PrecipitationUnitSymbol,
+  TemperatureUnitSymbol,
+  WindSpeedUnitSymbol,
+} from "./types";
 
 export function parseUnitType(value: string | null) {
   if (value === UnitType.Imperial) return UnitType.Imperial;
   return UnitType.Metric;
+}
+
+export function parseTemperatureUnitSymbol(
+  symbol: string,
+): TemperatureUnitSymbol {
+  return symbol === "°F" ? symbol : "°C";
+}
+
+export function parseWindSpeedUnitSymbol(symbol: string): WindSpeedUnitSymbol {
+  return symbol === "mp/h" ? symbol : "km/h";
+}
+
+export function parsePrecipitationUnitSymbol(
+  symbol: string,
+): PrecipitationUnitSymbol {
+  return symbol === "inch" ? symbol : "mm";
 }
