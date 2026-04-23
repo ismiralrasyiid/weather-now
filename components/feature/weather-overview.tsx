76224a26cb96fd2ae38a896a1e9ae19294013cff
ismiralrasyiid <ismiralrasyiid@gmail.com>
2026-04-23 23:25:24 +0700

    style(weather-overview): move overview text to top

--- components/feature/weather-overview.tsx
+++ components/feature/weather-overview.tsx
@@ -24,29 +24,34 @@ export default function WeatherOverview({
   return (
     <div
       className={clsx(
-        "flex h-hero-section flex-col items-center justify-center rounded-xl bg-[url(@/public/bg-today-small.svg)] bg-cover bg-center md:flex-row md:justify-between md:bg-[url(@/public/bg-today-large.svg)] md:p-5",
+        "h-hero-section rounded-xl bg-[url(@/public/bg-today-small.svg)] bg-cover bg-center p-4 md:bg-[url(@/public/bg-today-large.svg)] md:p-8",
         className,
       )}
     >
-      <div className="text-center md:text-left">
-        <h2 className="text-hero-section-city font-semibold capitalize">
-          {location}
-        </h2>
-        <p className="text-text-secondary">{date}</p>
-      </div>
-      <div className="flex items-center">
-        <Infotip description={weather.description} forOverview>
-          <Image
-            src={weather.image}
-            alt={weather.description}
-            width={110}
-            height={110}
-          />
-        </Infotip>
-        <p className="text-hero-section-degree font-semibold">
-          <span className="inline-block -skew-x-8">{temperature}</span>
-          &deg;
-        </p>
+      <div>
+        <div className="flex flex-col items-center md:flex-row md:justify-between">
+          <div className="text-center md:text-left">
+            <h2 className="text-hero-section-city font-semibold capitalize">
+              {location}
+            </h2>
+            <p className="text-text-secondary">{date}</p>
+          </div>
+          <div className="flex items-center">
+            <Infotip description={weather.description} forOverview>
+              <Image
+                className="size-22.5"
+                src={weather.image}
+                alt={weather.description}
+                width={90}
+                height={90}
+              />
+            </Infotip>
+            <p className="text-6xl leading-none font-semibold">
+              <span className="inline-block -skew-x-8">{temperature}</span>
+              &deg;
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
